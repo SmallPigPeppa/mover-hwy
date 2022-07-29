e24e2f895e0504bbcb6973f83e18add27e0a1769
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-07-29 13:32:59 +0800

    initial

--- thirdparty/scene_initialization/detection/run_pointrend_coco_2.sh
+++ thirdparty/scene_initialization/detection/run_pointrend_coco_2.sh
@@ -3,7 +3,8 @@ DIR=prox_qualitative
 IDX=1
 echo ${DIR}
 #cd /is/cluster/hyi/workspace/HCI/tool_scripts/detection/
-/home/hyi/anaconda3/envs/pt1.7_cu10.1_newestDetron2/bin/python test_det_pointrend.py \
+conda activate mover
+python test_det_pointrend.py \
 	--img-root=${DIR} \
   --out-img-root=${DIR}_pointrend_X_101_det_all \
 	--width=1920 \
