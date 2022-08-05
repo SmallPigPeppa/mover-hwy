f27796198d13160b932f0cfe6df59689ceaa9963
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-08-05 11:07:16 +0800

    initial

---
+++ thirdparty/HPS_initialization/POSA/run_input_lwz.sh
@@ -0,0 +1,17 @@
+conda activate POSA
+# input $1: the directory contains the pkl files which stores the smplx parameters.
+smplfx_out=/share/wenzhuoliu/code/mover-hwy/modified_smplifyx/results
+POSA_dir=/share/wenzhuoliu/code/mover-hwy/thirdparty/HPS_initialization/POSA_dir
+pkl_file_path=${smplfx_out}/split
+# pkl_file_path=$2/split
+rand_samples_dir=${smplfx_out}/posa_render_results
+save_dir=${smplfx_out}/posa_contact_npy_newBottom
+python src/gen_rand_samples.py \
+    --config cfg_files/contact.yaml \
+    --checkpoint_path $POSA_dir/trained_models/contact.pt \
+    --pkl_file_path ${pkl_file_path} \
+    --render 1 \
+    --num_rand_samples 1 \
+    --save 1 \
+    --rand_samples_dir ${rand_samples_dir} \
+    --save_dir ${save_dir}
