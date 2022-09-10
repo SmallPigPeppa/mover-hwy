cd8c586cb063f5853167b4d765d1979ffe33df74
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-09-10 11:10:34 +0800

    initial

---
+++ thirdparty/scene_initialization/detection/run_pointrend_human.sh
@@ -0,0 +1,17 @@
+DIR=/share/wenzhuoliu/code/mover-hwy/thirdparty/scene_initialization/prox_qualitative
+DIR=/share/wenzhuoliu/code/mover-lwz/input
+IDX=2
+echo ${DIR}
+cd ./detection/
+conda activate point-render
+python test_det_pointrend.py \
+	--img-root=${DIR} \
+  --out-img-root=${DIR}_pointrend_X_101_det_all \
+	--width=1920 \
+	--height=1080 \
+	--bbox-thr 0.55 \
+	--save \
+	--show \
+	--idx=${IDX}
+
+cd ..
